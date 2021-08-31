e18f905b920bf3fe06e190d055a9e2c19e6651a3
Encep Suryana <encep.suryanajr@gmail.com>
2021-08-31 17:28:28 +0700

    Add Register Contoller

---
+++ middleware/auth.js
@@ -0,0 +1,44 @@
+const connection = require("../koneksi");
+const mysql = require("mysql");
+const md5 = require("md5");
+const response = require("../res");
+const jwt = require("jsonwebtoken");
+const config = require("../config/secret");
+const ip = require("ip");
+
+//contoller Register
+exports.registration = function (req, res) {
+  var post = {
+    username: req.body.username,
+    email: req.body.email,
+    password: md5(req.body.password),
+    role: req.body.role,
+    tanggal_daftar: new Date(),
+  };
+
+  const query = `SELECT email from ?? WHERE ??`;
+  const table = ["user", "email", post.email];
+
+  query = mysql.format(query, table);
+
+  connection.query(query, function (error, rows) {
+    if (error) {
+      console.log(error);
+    } else {
+      if (rows.length == 0) {
+        var query = `INSERT INTO ?? SET ?`;
+        var table = ["user"];
+        query = mysql.format(query, table);
+        connection.query(query, post, function (error, rows) {
+          if (error) {
+            console.log(error);
+          } else {
+            response.ok("Success add new user Data", res);
+          }
+        });
+      } else {
+        response.ok("Email already register!");
+      }
+    }
+  });
+};
